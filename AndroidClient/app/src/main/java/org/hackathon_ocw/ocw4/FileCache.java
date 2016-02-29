ee900fba8b31c357dcc6f1a427c4707d63590692
luckyyd <luckyyd@tju.edu.cn>
2016-02-29 17:04:00 +0800

    Display open courses list from json web file
    tested on ZTE

---
+++ AndroidClient/app/src/main/java/org/hackathon_ocw/ocw4/FileCache.java
@@ -0,0 +1,36 @@
+package org.hackathon_ocw.ocw4;
+
+import java.io.File;
+import android.content.Context;
+
+/**
+ * Created by dianyang on 2016/2/29.
+ */
+public class FileCache {
+    private File cacheDir;
+
+    public FileCache(Context context){
+        if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED))
+            cacheDir=new File(android.os.Environment.getExternalStorageDirectory(),"CourseList");
+        else
+            cacheDir=context.getCacheDir();
+        if(!cacheDir.exists())
+            cacheDir.mkdirs();
+    }
+
+    public File getFile(String url){
+
+        String filename=String.valueOf(url.hashCode());
+        File f = new File(cacheDir, filename);
+        return f;
+
+    }
+
+    public void clear(){
+        File[] files=cacheDir.listFiles();
+        if(files==null)
+            return;
+        for(File f:files)
+            f.delete();
+    }
+}
