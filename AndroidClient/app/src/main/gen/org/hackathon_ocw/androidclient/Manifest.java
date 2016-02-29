bcbf2e4b5ebc7699533cc2829c02ebb487a63360
luckyyd <luckyyd@tju.edu.cn>
2016-02-29 17:00:05 +0800

    Revert "Display open course list from web"
    This reverts commit 45047abe8b8a1f10351425fd73e88a9619c5a023.

--- AndroidClient/app/src/main/gen/org/hackathon_ocw/androidclient/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package org.hackathon_ocw.androidclient;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
