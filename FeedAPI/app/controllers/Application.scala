f8ac128dda9d44ef26ca0a63a6121fd7f94b5bc9
刘杨 <foamliu@yeah.net>
2016-03-09 16:51:45 +0800

    服务端：修复日志警报
    [warn] - application - Logger configuration in conf files is deprecated
    and has no effect. Use a logback configuration file instead.

--- FeedAPI/app/controllers/Application.scala
+++ FeedAPI/app/controllers/Application.scala
@@ -143,30 +143,6 @@ class Application extends Controller {
         
         Ok(Json.stringify(json))
     }
-  
-    def addPreference(id: Long) = Action(parse.json) {
-
-        val userID: Long = id
-        Logger.debug (userID toString)
-
-        request =>
-        {
-            val json: JsValue = request.body
-            
-            val user_id = (json \ "user_id").as[Long]
-            val item_id = (json \ "item_id").as[Long]
-            val pref = (json \ "pref").as[Float]
-            Logger.info("%d,%d,%f".format(user_id,item_id,pref))
-          
-            val file = Application.getPrefFile
-            val bw = new BufferedWriter(new FileWriter(file, true))
-            bw.write("%d,%d,%f".format(user_id,item_id,pref))
-            bw.newLine()
-            bw.close()
-          
-            Ok(user_id.toString)
-        }
-    }
 
 }
 
