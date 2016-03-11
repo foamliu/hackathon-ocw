7e05f4c88bbc7400134d34a6d7d7dafa3ec7a872
刘杨 <foamliu@yeah.net>
2016-03-11 23:34:52 +0800

    服务端：用户未登陆前可以通过 DEVICE ID 得到一个自增的长整数 USER ID

--- FeedAPI/app/backend/UserRepo.scala
+++ FeedAPI/app/backend/UserRepo.scala
@@ -1,38 +1,46 @@
 package backend
 
-import play.api.libs.json.{JsObject, Json}
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import play.api.libs.json.JsObject
 import play.modules.reactivemongo.ReactiveMongoApi
 import play.modules.reactivemongo.json.collection.JSONCollection
-import reactivemongo.api.ReadPreference
+import play.api.libs.json.Json
 import reactivemongo.api.commands.WriteResult
 import reactivemongo.bson.{BSONObjectID, BSONDocument}
-
-import scala.concurrent.{ExecutionContext, Future}
+import reactivemongo.bson.BSONDocumentReader
+import reactivemongo.api.ReadPreference
+import scala.util.Failure
+import scala.util.Success
+import scala.concurrent.Await
 
 trait UserRepo {
-  def list()(implicit ec: ExecutionContext): Future[List[JsObject]]
-  
-  def find(query: JsObject)(implicit ec: ExecutionContext): Future[List[JsObject]]
+    def list()(implicit ec: ExecutionContext): Future[List[JsObject]]
 
-  def update(selector: BSONDocument, update: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult]
+    def find(selector: JsObject)(implicit ec: ExecutionContext): Future[List[JsObject]]
 
-  def remove(document: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult]
+    def update(selector: BSONDocument, update: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult]
 
-  def save(document: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult]
+    def remove(document: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult]
+
+    def save(document: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult]
+    
+    def nextID()(implicit ec: ExecutionContext): Long
 }
 
 class UserMongoRepo(reactiveMongoApi: ReactiveMongoApi) extends UserRepo {
     // BSON-JSON conversions
     import play.modules.reactivemongo.json._
 
-    protected def collection =
-        reactiveMongoApi.db.collection[JSONCollection]("users")
+    protected def collection = reactiveMongoApi.db.collection[JSONCollection]("users")
 
     def list()(implicit ec: ExecutionContext): Future[List[JsObject]] =
         collection.find(Json.obj()).cursor[JsObject](ReadPreference.Primary).collect[List]()
-    
-    def find(query: JsObject)(implicit ec: ExecutionContext): Future[List[JsObject]] =
-        collection.find(query).cursor[JsObject](ReadPreference.Primary).collect[List]()
+
+    def find(selector: JsObject)(implicit ec: ExecutionContext): Future[List[JsObject]] =
+        collection.find(selector).cursor[JsObject](ReadPreference.Primary).collect[List]()
 
     def update(selector: BSONDocument, update: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult] = collection.update(selector, update)
 
@@ -40,4 +48,22 @@ class UserMongoRepo(reactiveMongoApi: ReactiveMongoApi) extends UserRepo {
 
     def save(document: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult] =
         collection.update(BSONDocument("_id" -> document.get("_id").getOrElse(BSONObjectID.generate)), document, upsert = true)
+
+    def nextID()(implicit ec: ExecutionContext): Long = {    
+        def counters = reactiveMongoApi.db.collection[JSONCollection]("counters")
+       
+        val jsonFuture = counters.findAndUpdate(
+                BSONDocument("_id" -> "userid"), 
+                BSONDocument("$inc" -> BSONDocument("seq" -> 1)),
+                fetchNewObject = true
+                ).map(_.value)
+                
+        val json: Option[JsObject] = Await.result(jsonFuture, Duration.Inf)
+        
+        json match {
+            case Some(json) => (json \ "seq").as[Long]
+            case None => 0
+        }
+    }
+
 }
--- FeedAPI/app/controllers/Users.scala
+++ FeedAPI/app/controllers/Users.scala
@@ -1,47 +1,67 @@
 package controllers
 
 import javax.inject.Inject
-
 import play.api._
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.json._ // JSON library
-
+import play.api.libs.json._
 import reactivemongo.bson.{ BSONObjectID, BSONDocument }
 import reactivemongo.core.actors.Exceptions.PrimaryUnavailableException
 import reactivemongo.api.commands.WriteResult
-
 import play.modules.reactivemongo.{
-  MongoController, ReactiveMongoApi, ReactiveMongoComponents
+    MongoController,
+    ReactiveMongoApi,
+    ReactiveMongoComponents
 }
+import scala.util.Failure
+import scala.util.Success
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import scala.concurrent.Future
 
 class Users @Inject() (val reactiveMongoApi: ReactiveMongoApi)
-    extends Controller with MongoController with ReactiveMongoComponents {
-    
+        extends Controller with MongoController with ReactiveMongoComponents {
+
     import controllers.UserFields._
-    
-    def userRepo = new backend.UserMongoRepo(reactiveMongoApi)    
-    
-    def list = Action.async {implicit request =>
+
+    def userRepo = new backend.UserMongoRepo(reactiveMongoApi)
+
+    def list = Action.async { implicit request =>
         userRepo.list()
             .map(users => Ok(Json.toJson(users.reverse)))
-            .recover {case PrimaryUnavailableException => InternalServerError("Please install MongoDB")}
+            .recover { case PrimaryUnavailableException => InternalServerError("Please install MongoDB") }
     }
-    
-    def auth = Action.async(BodyParsers.parse.json) { implicit request =>
-        val deviceid = (request.body \ DeviceID).as[String]
-        var userID = 0
-        
-        val users = userRepo.find(Json.obj(DeviceID -> deviceid))
-            .map(users => Ok(Json.toJson(users.reverse)))
-            .recover {case PrimaryUnavailableException => InternalServerError("Please install MongoDB")}
 
-        userRepo.save(BSONDocument(
-                UserID -> userID,
-                DeviceID -> deviceid
-            )).map(le => Redirect(routes.Users.list()))
+    def register = Action.async(BodyParsers.parse.json) { implicit request =>
+        val deviceid = (request.body \ DeviceID).as[String]
+        Logger.info(deviceid)
+        
+        var found: Boolean = false
+        val userIDFuture: Future[Long] = userRepo
+            .find(Json.obj(DeviceID -> deviceid))
+            .map(users => users.size match {
+                    case 0 => userRepo.nextID
+                    case _ => found = true; (users(0) \ UserID).as[Long] 
+                })
+        
+        val userID: Long = Await.result(userIDFuture, Duration.Inf)
+        Logger.info(userID toString)
+        Logger.info(found toString)
+        
+        if (!found)
+        {
+            userRepo
+                .save(BSONDocument(
+                    UserID -> userID,
+                    DeviceID -> deviceid))
+                .map(_ => Ok(Json.obj("userid" -> userID)))
+        }
+        else
+        {
+            Future(Ok(Json.obj("userid" -> userID)))
+        }
     }
-    
+
     def update(id: Long) = Action.async(BodyParsers.parse.json) { implicit request =>
         val openid = (request.body \ OpenID).as[String]
         val nickname = (request.body \ Nickname).as[String]
@@ -50,28 +70,29 @@ class Users @Inject() (val reactiveMongoApi: ReactiveMongoApi)
         val city = (request.body \ City).as[String]
         val country = (request.body \ Country).as[String]
         val headimgurl = (request.body \ HeadImgUrl).as[String]
-        
-        userRepo.update(BSONDocument(UserID -> BSONObjectID(id toString)), BSONDocument("$set" -> BSONDocument(
-                OpenID -> openid,
-                Nickname -> nickname,
-                Sex -> sex,
-                Province -> province,
-                City -> city,
-                Country -> country,
-                HeadImgUrl -> headimgurl
-            ))).map(le => Ok(Json.obj("success" -> le.ok)))
-    
+
+        userRepo.update(
+                BSONDocument(UserID -> BSONObjectID(id toString)), 
+                BSONDocument("$set" -> BSONDocument(
+                    OpenID -> openid,
+                    Nickname -> nickname,
+                    Sex -> sex,
+                    Province -> province,
+                    City -> city,
+                    Country -> country,
+                    HeadImgUrl -> headimgurl))).map(le => Ok(Json.obj("success" -> le.ok)))
+
     }
 }
 
 object UserFields {
-  val UserID = "_id"
-  val DeviceID = "deviceid"
-  val OpenID = "openid"
-  val Nickname = "nickname"
-  val Sex = "sex"
-  val Province = "province"
-  val City = "city"
-  val Country = "country"
-  val HeadImgUrl = "headimgurl"
+    val UserID = "_id"
+    val DeviceID = "deviceid"
+    val OpenID = "openid"
+    val Nickname = "nickname"
+    val Sex = "sex"
+    val Province = "province"
+    val City = "city"
+    val Country = "country"
+    val HeadImgUrl = "headimgurl"
 }
