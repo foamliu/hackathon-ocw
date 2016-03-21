9696701e96850e6c34c558486cc04dd05ef6446b
刘杨 <foamliu@yeah.net>
2016-03-21 10:38:39 +0800

    服务端：把推荐算法换成 SVD

--- FeedAPI/app/controllers/Application.scala
+++ FeedAPI/app/controllers/Application.scala
@@ -1,23 +1,14 @@
 package controllers
 
-import java.io.BufferedWriter
-import java.io.File
-import java.io.FileWriter
-import java.util.List
-
 import scala.collection.JavaConversions.asScalaBuffer
-import scala.collection.JavaConversions.bufferAsJavaList
 import scala.io.Codec.string2codec
 import scala.io.Source
 
-import org.apache.mahout.cf.taste.impl.model.GenericBooleanPrefDataModel
-import org.apache.mahout.cf.taste.impl.model.file.FileDataModel
-import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood
-import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender
-import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity
-import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood
+import org.apache.mahout.cf.taste.common.NoSuchUserException
+import org.apache.mahout.cf.taste.impl.model.mongodb.MongoDBDataModel
+import org.apache.mahout.cf.taste.impl.recommender.svd.ALSWRFactorizer
+import org.apache.mahout.cf.taste.impl.recommender.svd.SVDRecommender
 import org.apache.mahout.cf.taste.recommender.Recommender
-import org.apache.mahout.cf.taste.similarity.UserSimilarity
 
 import play.api.Logger
 import play.api.Play
@@ -37,6 +28,7 @@ object Application {
     private val item_file = "app/assets/jsons/items.json"
 
     private var courses: Seq[Course] = null
+    private var recommender: Recommender = null
 
     private def getCourses(): Seq[Course] = {
 
@@ -48,68 +40,47 @@ object Application {
         }
 
         courses
-
     }
 
-    def getPrefFile(): File = {
-
-        var file: File = new File(pref_file)
-
-        Logger.debug("Pref file's absolute path = " + file.getAbsolutePath)
-        Logger.debug("Pref file exists: " + file.exists)
-        Logger.debug("Play.application.path: " + Play.application.path)
-        Logger.debug("Play.application.getFile.getAbsolutePath: " + Play.application.getFile("/").getAbsolutePath)
-
-        if (!file.exists()) {
-            val bw = new BufferedWriter(new FileWriter(file))
-            //val items: Seq[Course]  = getCourses
-            //items.foreach(i => {bw.write("%d,%d,%f".format(0, i.itemID, 1.0));bw.newLine()})
-            bw.write("1,1,1.0")
-            bw.newLine()
-            bw.close
-
-            file = new File(pref_file)
+    val ip = Play.current.configuration.getString("mongodb.ip")
+    val port = Play.current.configuration.getInt("mongodb.port")
+    val db = Play.current.configuration.getString("mongodb.db")
+    val numFeatures = 3
+    val lambda = 0.05f
+    val numIterations = 50
+            
+    private def getRecommender(): Recommender = {
+        
+        if (recommender == null) {
+            var model = new MongoDBDataModel(ip.get, port.get, db.get, "ratings", true, true, null)
+            recommender = new SVDRecommender(model, new ALSWRFactorizer(model, numFeatures, lambda, numIterations))
         }
 
-        file
+        recommender
     }
 
     private def recommend(userID: Long): List[Long] = {
 
-        Logger.debug("UserID: " + userID)
+        var list = List[Long]()
 
-        val file: File = getPrefFile()
+        try {
+            var recommendations = getRecommender.recommend(userID, howMany)
 
-        Logger.debug("Pref file exists: " + file.exists)
+            for (r <- recommendations) list = r.getItemID :: list
 
-        var model: GenericBooleanPrefDataModel = new GenericBooleanPrefDataModel(
-            GenericBooleanPrefDataModel.toDataMap(new FileDataModel(file)))
+        } catch {
+            case nsue: NoSuchUserException => Logger.warn(nsue.getMessage)
+            case e: Exception              => Logger.warn(e.getMessage)
+        }
 
-        Logger.debug("NumItems = " + model.getNumItems + " NumUsers = " + model.getNumUsers)
-        Logger.debug("UserIDs: " + model.getUserIDs)
-
-        var similarity: UserSimilarity = new LogLikelihoodSimilarity(model)
-        var neighborhood: UserNeighborhood = new NearestNUserNeighborhood(n, similarity, model);
-
-        var recommender: Recommender = new GenericUserBasedRecommender(model, neighborhood, similarity)
-        var recommendations = recommender.recommend(userID, howMany)
-        Logger.debug("NumRecommendations: " + recommendations.size)
-
-        for (r <- recommendations) yield r.getItemID
-
+        list
     }
 
     private def getCandidates(userID: Long): Seq[Course] = {
 
         val items: Seq[Course] = getCourses
-        Logger.debug(items.size + " items are loaded successfully.")
-
         val itemIDs: List[Long] = recommend(userID)
-        Logger.debug("NumItemIDs: " + itemIDs.size)
-        Logger.debug("ItemIDs: " + itemIDs)
-
         val candidates: Seq[Course] = items.filter(i => itemIDs.contains(i.itemID))
-        Logger.debug("NumCandidates: " + candidates.size)
 
         if (candidates.size > 0) {
             candidates
@@ -123,13 +94,11 @@ object Application {
 class Application extends Controller {
 
     def index = Action {
-        //Ok(views.html.index("Your new application is ready."))
         Ok("Your new application is ready.")
     }
 
-    def getCandidates(id: Long) = Action {
+    def getCandidates(userID: Long) = Action {
 
-        val userID: Long = id
         Logger.debug(userID toString)
 
         val candidates: Seq[Course] = Application.getCandidates(userID)
@@ -152,7 +121,7 @@ class Application extends Controller {
                 Ok("Ok")
             }
     }
-    
+
     def echo(message: String) = Action {
         Ok(message)
     }
