24cf8e2125f0c2349082d79b692e68b67887da8f
刘杨 <foamliu@yeah.net>
2016-03-21 15:42:27 +0800

    服务端：模型定期从 MongoDB中加载数据
    加个后台任务每小时执行

--- FeedAPI/app/Global.scala
+++ FeedAPI/app/Global.scala
@@ -1,54 +1,73 @@
+import scala.concurrent.duration.DurationInt
+
 import javax.inject.Inject
-
-import play.api.Play.current
+import play.api.Application
+import play.api.GlobalSettings
+import play.api.Logger
+import play.api.libs.concurrent.Akka
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.iteratee.Enumerator
 import play.api.libs.json.Json
-import play.api.{ Logger, Application, GlobalSettings }
-
+import play.api.libs.json.Json.toJsFieldJsValueWrapper
 import play.modules.reactivemongo.ReactiveMongoApi
 import play.modules.reactivemongo.json.collection.JSONCollection
 
-class Global @Inject() (
-  val reactiveMongoApi: ReactiveMongoApi) extends GlobalSettings {
+object Global extends GlobalSettings {
 
-  def collection = reactiveMongoApi.db.collection[JSONCollection]("users")
+    override def onStart(app: Application) {
 
-  val users = List(
-    Json.obj(
-      "userid" -> 88,
-      "openid" -> "OPENID",
-      "nickname" -> "foamliu",
-      "sex" -> "1",
-      "province" -> "Shanghai",
-      "city" -> "Shanghai",
-      "country" -> "CN",
-      "headimgurl" -> "http://wx.qlogo.cn/mmopen/g3MonUZtNHkdmzicIlibx6iaFqAc56vxLSUfpb6n5WKSYVY0ChQKkiaJSgQ1dZuTOgvLLrhJbERQQ4eMsv84eavHiaiceqxibJxCfHe/46"
-    ),
-    Json.obj(
-      "userid" -> 15,
-      "openid" -> "OPENID",
-      "nickname" -> "diyan",
-      "sex" -> "1",
-      "province" -> "Shanghai",
-      "city" -> "Shanghai",
-      "country" -> "CN",
-      "headimgurl" -> "http://wx.qlogo.cn/mmopen/g3MonUZtNHkdmzicIlibx6iaFqAc56vxLSUfpb6n5WKSYVY0ChQKkiaJSgQ1dZuTOgvLLrhJbERQQ4eMsv84eavHiaiceqxibJxCfHe/46"
-    )
-  )
+        play.api.Play.mode(app) match {
+            case play.api.Mode.Test => // do not schedule anything for Test
+            case _                  => refreshDaemon(app)
+        }
 
-  override def onStart(app: Application) {
-    Logger.info("Application has started")
-
-    collection.bulkInsert(users.toStream, ordered = true).
-      foreach(i => Logger.info("Database was initialized"))
-  }
-
-  override def onStop(app: Application) {
-    Logger.info("Application shutdown...")
-
-    collection.drop().onComplete {
-      case _ => Logger.info("Database collection dropped")
     }
-  }
+
+    def refreshDaemon(app: Application) = {
+        Logger.info("Scheduling the data model refresh daemon")
+        Akka.system(app).scheduler.schedule(0 seconds, 1 hours) {
+            Logger.info("refreshing data model")
+        }
+    }
+
+}
+
+class Global @Inject() (
+        val reactiveMongoApi: ReactiveMongoApi) extends GlobalSettings {
+
+    def collection = reactiveMongoApi.db.collection[JSONCollection]("users")
+
+    val users = List(
+        Json.obj(
+            "userid" -> 88,
+            "openid" -> "OPENID",
+            "nickname" -> "foamliu",
+            "sex" -> "1",
+            "province" -> "Shanghai",
+            "city" -> "Shanghai",
+            "country" -> "CN",
+            "headimgurl" -> "http://wx.qlogo.cn/mmopen/g3MonUZtNHkdmzicIlibx6iaFqAc56vxLSUfpb6n5WKSYVY0ChQKkiaJSgQ1dZuTOgvLLrhJbERQQ4eMsv84eavHiaiceqxibJxCfHe/46"),
+        Json.obj(
+            "userid" -> 15,
+            "openid" -> "OPENID",
+            "nickname" -> "diyan",
+            "sex" -> "1",
+            "province" -> "Shanghai",
+            "city" -> "Shanghai",
+            "country" -> "CN",
+            "headimgurl" -> "http://wx.qlogo.cn/mmopen/g3MonUZtNHkdmzicIlibx6iaFqAc56vxLSUfpb6n5WKSYVY0ChQKkiaJSgQ1dZuTOgvLLrhJbERQQ4eMsv84eavHiaiceqxibJxCfHe/46"))
+
+    override def onStart(app: Application) {
+        Logger.info("Application has started")
+
+        collection.bulkInsert(users.toStream, ordered = true).
+            foreach(i => Logger.info("Database was initialized"))
+    }
+
+    override def onStop(app: Application) {
+        Logger.info("Application shutdown...")
+
+        collection.drop().onComplete {
+            case _ => Logger.info("Database collection dropped")
+        }
+    }
 }
