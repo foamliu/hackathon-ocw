f2793ddfef6926be9314385a6ebf7ab6156850b3
root <root@foamliu-centos.novalocal>
2016-03-31 10:09:33 +0000

    fix tabs

--- Crawler/yixi/yixi/spiders/spider_yixi.py
+++ Crawler/yixi/yixi/spiders/spider_yixi.py
@@ -27,14 +27,14 @@ class YixiSpider(scrapy.Spider):
             hxs = scrapy.Selector(text = self.driver.page_source)
             
             for sel in hxs.xpath('//div[@class="mainBody"]/section/ul/li'):
-				item = YixiItem()
- 				item['title'] = sel.xpath('div/span[@class="videoTitle"]/text()').extract()
-	            item['description'] = sel.xpath('//div/span[@class="speakerDescription"]/text()').extract()
- 				item['piclink'] = sel.xpath('//div[@class="videoContent"]/@style').extract()
-	            item['courselink'] = sel.xpath('//div[@class="videoCover"]/a/@href').extract()
-	            item['duration'] = sel.xpath('li[@class="playTime"]').extract()
-				item['source'] = "yixi"
+                item = YixiItem()
+                item['title'] = sel.xpath('div/span[@class="videoTitle"]/text()').extract()
+                item['description'] = sel.xpath('//div/span[@class="speakerDescription"]/text()').extract()
+                item['piclink'] = sel.xpath('//div[@class="videoContent"]/@style').extract()
+                item['courselink'] = sel.xpath('//div[@class="videoCover"]/a/@href').extract()
+                item['duration'] = sel.xpath('li[@class="playTime"]').extract()
+                item['source'] = "yixi"
                 yield item
             
             page = page + 1        
-		
+
