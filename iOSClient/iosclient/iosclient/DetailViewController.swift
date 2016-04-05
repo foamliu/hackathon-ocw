4619cb831db3d41cdf91040bff7e244438187494
luckyyd <luckyyd@tju.edu.cn>
2016-04-05 18:14:17 +0800

    客户端：子页面播放视频

--- iOSClient/iosclient/iosclient/DetailViewController.swift
+++ iOSClient/iosclient/iosclient/DetailViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 
 class DetailViewController: UIViewController {
     
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    var selectedTitle: String!
+    var videoUrl: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        labelTitle.text = selectedTitle
+                
+        //play online video
+        if (videoUrl != nil){
+            let url = NSURL.fileURLWithPath(videoUrl)
+            let player = AVPlayer(URL: url)
+            let playerViewController = AVPlayerViewController()
+            playerViewController.player = player
+            
+            playerViewController.view.frame = CGRectMake(10, 110, 360, 300)
+            self.view.addSubview(playerViewController.view)
+            self.addChildViewController(playerViewController)
+            
+            player.play()
+        }
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
     
 }
