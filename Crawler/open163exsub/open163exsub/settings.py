9b66d00a88b004f7296095cff4db69ce91711076
root <foamliu@yeah.net>
2016-04-14 10:27:10 +0000

    fix a crawler bug

--- Crawler/open163exsub/open163exsub/settings.py
+++ Crawler/open163exsub/open163exsub/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'open163exsub.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'open163exsub (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/BuildID) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS=32
