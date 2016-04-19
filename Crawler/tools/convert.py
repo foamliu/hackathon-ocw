311a870ab905bfc109c8ee0c645cfc0ad2d1f809
刘杨 <foamliu@yeah.net>
2016-04-19 21:40:33 +0800

    merge opensina to items.json

--- Crawler/tools/convert.py
+++ Crawler/tools/convert.py
@@ -12,14 +12,14 @@ def get_duration(olist, url):
 #input_file = open(r'C:\Users\foamliu.FAREAST\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\items.json', "r")
 #output_file = codecs.open(r'C:\Users\foamliu.FAREAST\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\output.json', "w", encoding="utf-8")
 
-input_file_1 = open(r'C:\Users\foamliu.FAREAST\Documents\GitHub\hackathon-ocw\Crawler\infoqsub\out.json', "r", encoding="utf-8")
-input_file_2 = open(r'C:\Users\foamliu.FAREAST\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\items.json', "r", encoding="utf-8")
-output_file = codecs.open(r'C:\Users\foamliu.FAREAST\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\output.json', "w", encoding="utf-8")
+input_file_1 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\Crawler\opensina\out.json', "r", encoding="utf-8")
+input_file_2 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\items.json', "r", encoding="utf-8")
+output_file = codecs.open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\output.json', "w", encoding="utf-8")
 
 olist = json.load(input_file_2, encoding='utf-8')
 
 lines = input_file_1.readlines()
-i = 5637
+i = 6992
 for line in lines:
     item = json.loads(line)
     item['item_id'] = i
