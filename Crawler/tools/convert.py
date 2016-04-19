144832036d1453cdc841fb5c0a8dee2734f7df28
刘杨 <foamliu@yeah.net>
2016-04-19 14:35:13 +0800

    update python script to extend items.json

--- Crawler/tools/convert.py
+++ Crawler/tools/convert.py
@@ -9,24 +9,23 @@ def get_duration(olist, url):
         if (item['courselink']) == url: return item['duration']
     return ''
 
-#input_file = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\Crawler\opensina\items.json', "r")
-#output_file = codecs.open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\Crawler\opensina\output.json', "w", encoding="utf-8")
+#input_file = open(r'C:\Users\foamliu.FAREAST\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\items.json', "r")
+#output_file = codecs.open(r'C:\Users\foamliu.FAREAST\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\output.json', "w", encoding="utf-8")
 
-input_file_1 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\Crawler\open163exsub\out.json', "r", encoding="utf-8")
-input_file_2 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\items.json', "r", encoding="utf-8")
-output_file = codecs.open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\Crawler\open163exsub\output.json', "w", encoding="utf-8")
+input_file_1 = open(r'C:\Users\foamliu.FAREAST\Documents\GitHub\hackathon-ocw\Crawler\infoqsub\out.json', "r", encoding="utf-8")
+input_file_2 = open(r'C:\Users\foamliu.FAREAST\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\items.json', "r", encoding="utf-8")
+output_file = codecs.open(r'C:\Users\foamliu.FAREAST\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\output.json', "w", encoding="utf-8")
 
 olist = json.load(input_file_2, encoding='utf-8')
-output = []
+
 lines = input_file_1.readlines()
-i = 1
+i = 5637
 for line in lines:
     item = json.loads(line)
     item['item_id'] = i
-    item['duration'] = get_duration(olist, item['courselink'])
-    output.append(item)
+    item['duration'] = ''
+    olist.append(item)
     i += 1
 
 
-json.dump(output ,output_file, indent=4,ensure_ascii=False)
-
+json.dump(olist ,output_file, indent=4,ensure_ascii=False)
