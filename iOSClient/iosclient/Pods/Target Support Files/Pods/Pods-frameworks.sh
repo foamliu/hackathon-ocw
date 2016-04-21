36d7cb38bdb69300cd69d55537c060503326912d
luckyyd <luckyyd@tju.edu.cn>
2016-04-21 20:57:42 +0800

    客户端(iOS):增加rating bar库

--- iOSClient/iosclient/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ iOSClient/iosclient/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -85,11 +85,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/Alamofire.framework"
+  install_framework "Pods/Cosmos.framework"
   install_framework "Pods/SDWebImage.framework"
   install_framework "Pods/TagListView.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/Alamofire.framework"
+  install_framework "Pods/Cosmos.framework"
   install_framework "Pods/SDWebImage.framework"
   install_framework "Pods/TagListView.framework"
 fi
