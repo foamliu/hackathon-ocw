b4734d337e661884965fbfcef9cd4d13603f01b0
刘杨 <foamliu@yeah.net>
2016-04-26 22:53:04 +0800

    爬虫：重刷一遍果壳 上传 并enable

--- Crawler/tools/convert2.py
+++ Crawler/tools/convert2.py
@@ -2,13 +2,13 @@
 import json
 import codecs
 
-input_file = open(r'C:\Users\foamliu.FAREAST\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\items.json', "r", encoding="utf-8")
-output_file = codecs.open(r'C:\Users\foamliu.FAREAST\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\output.json', "w", encoding="utf-8")
+input_file = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\items.json', "r", encoding="utf-8")
+output_file = codecs.open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\output.json', "w", encoding="utf-8")
 
 items = json.load(input_file, encoding='utf-8')
 
 for item in items:
-    if item['source'] == '果壳网':
+    if item['source'] == '新浪公开课':
         item['enabled'] = False
     else:
         item['enabled'] = True
