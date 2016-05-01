4da4fb53376ef38906786fdec4c285f3c9747414
刘杨 <foamliu@yeah.net>
2016-05-01 08:20:30 +0800

    refresh yixi

--- Crawler/tools/convert.py
+++ Crawler/tools/convert.py
@@ -18,14 +18,14 @@ def downloaded(items, link):
             return True
     return False
 
-input_file_1 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\Crawler\guokr\out.json', "r", encoding="utf-8")
+input_file_1 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\Crawler\yixi\out.json', "r", encoding="utf-8")
 input_file_2 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\items.json', "r", encoding="utf-8")
 output_file = codecs.open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\output.json', "w", encoding="utf-8")
 
 items = json.load(input_file_2, encoding='utf-8')
 
 lines = input_file_1.readlines()
-i = 33005
+i = 33014
 for line in lines:
     line = line.replace('\\','\\\\')
     #print(line)
