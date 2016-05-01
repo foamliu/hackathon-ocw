6b6ed9be7651166dbad32049e8990863ecd4ca86
刘杨 <foamliu@yeah.net>
2016-05-02 07:16:17 +0800

    爬虫：一席的学校名应为“一席”而非地名

--- Crawler/tools/convert2.py
+++ Crawler/tools/convert2.py
@@ -8,9 +8,8 @@ output_file = codecs.open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\FeedAPI
 items = json.load(input_file, encoding='utf-8')
 
 for item in items:
-    if item['source'] == '新浪公开课':
-        item['enabled'] = False
-    else:
-        item['enabled'] = True
+    if item['source'] == '一席':
+        item['school'] = '一席'
+
 
 json.dump(items, output_file, indent=4, ensure_ascii=False, sort_keys=True)
