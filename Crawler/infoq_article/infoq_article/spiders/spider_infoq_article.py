80d2e5eae7f6521201fe22c61a56725b711777c9
root <foamliu@yeah.net>
2016-05-05 16:54:05 -0400

    update infoq articles

--- Crawler/infoq_article/infoq_article/spiders/spider_infoq_article.py
+++ Crawler/infoq_article/infoq_article/spiders/spider_infoq_article.py
@@ -22,6 +22,7 @@ class InfoqSpider(scrapy.Spider):
     name = 'infoq_article'
     allowed_domains = ["www.infoq.com"]
     start_urls = ["http://www.infoq.com/cn/articles"]
+    out = []
 
     def __init__(self):
         scrapy.Spider.__init__(self)
@@ -57,27 +58,34 @@ class InfoqSpider(scrapy.Spider):
         while True:
 
             hxs = scrapy.Selector(text = self.driver.page_source)
-            for info in hxs.xpath('/html/body/div[1]/div/ul[2]/li'):
+            print (hxs)
+            for info in hxs.xpath('/html/body/div[1]/ul[2]/li'):
+                print 'info'
                 item = InfoqArticleItem()
                 link = cleanse(info.xpath('a[1]/@href').extract())
                 link = 'http://www.infoq.com{0}'.format(link)
+                print (link)
                 if self.downloaded(link): return
+                print (self.downloaded(link))
                 item['link'] = link
-                item['title'] = cleanse(hxs.xpath('a[1]/text()').extract())
-                item['description'] = cleanse(hxs.xpath('a[2]/text()').extract())
-                item['piclink'] = cleanse(hxs.xpath('a[2]/img/@src').extract())
+                item['title'] = cleanse(info.xpath('a[1]/text()').extract())
+                item['description'] = cleanse(info.xpath('a[2]/text()').extract())
+                item['piclink'] = cleanse(info.xpath('a[2]/img/@src').extract())
                 item['courselink'] = u''
                 item['source'] = u'InfoQ'
                 item['school'] = u'InfoQ'
-                item['instructor'] = cleanse(hxs.xpath('span/a/text()').extract())
+                item['instructor'] = cleanse(info.xpath('span/a/text()').extract())
                 item['language'] = u'中文'
                 item['tags'] = u'InfoQ'
-                p_year = cleanse(hxs.xpath('ul/li[1]/text()').extract())
-                p_month = cleanse(hxs.xpath('ul/li[2]/text()').extract())
-                p_day = cleanse(hxs.xpath('ul/li[3]/text()').extract())
+                p_year = cleanse(info.xpath('ul/li[1]/text()').extract())
+                p_month = cleanse(info.xpath('ul/li[2]/text()').extract())
+                p_day = cleanse(info.xpath('ul/li[3]/text()').extract())
                 item['posted'] = u'{0} {1} {2}'.format(p_year, p_month, p_day)
+                print u'{0} {1} {2}'.format(p_year, p_month, p_day)
                 item['crawled'] = time.strftime('%Y-%m-%d %H:%M')
                 yield item
+
+            break
 
             next = self.driver.find_element_by_xpath('/html/body/div[1]/ul[1]/li[2]/a')
             aclass = next.get_attribute('class')
