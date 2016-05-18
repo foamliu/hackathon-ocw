0b28a546edaa21f14a6d0a62128dfcc98d8ef3fd
刘杨 <foamliu@yeah.net>
2016-05-19 07:42:15 +0800

    refresh guokr

--- Crawler/tools/convert.py
+++ Crawler/tools/convert.py
@@ -24,14 +24,14 @@ def getPos(items, link):
             return i
     return -1
 
-input_file_1 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\Crawler\infoq_article\out.json', "r", encoding="utf-8")
+input_file_1 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\Crawler\guokr\out.json', "r", encoding="utf-8")
 input_file_2 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\items.json', "r", encoding="utf-8")
 output_file = codecs.open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\output.json', "w", encoding="utf-8")
 
 items = json.load(input_file_2, encoding='utf-8')
 
 lines = input_file_1.readlines()
-i = 34071
+i = 36620
 for line in lines:
     line = line.replace('\\','\\\\')
     #print(line)
