2db38e77352139e3b78386042a582930ebd04e79
刘杨 <foamliu@yeah.net>
2016-05-21 08:01:44 +0800

    爬虫：刷InfoQ

--- Crawler/tools/convert.py
+++ Crawler/tools/convert.py
@@ -24,18 +24,19 @@ def getPos(items, link):
             return i
     return -1
 
-input_file_1 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\Crawler\guokr\out.json', "r", encoding="utf-8")
+input_file_1 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\Crawler\infoq\out.json', "r", encoding="utf-8")
 input_file_2 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\items.json', "r", encoding="utf-8")
 output_file = codecs.open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\output.json', "w", encoding="utf-8")
 
 items = json.load(input_file_2, encoding='utf-8')
 
 lines = input_file_1.readlines()
-i = 36620
+i = 36662
 for line in lines:
     line = line.replace('\\','\\\\')
     #print(line)
     item = json.loads(line)
+    print(item)
     pos = getPos(items, item['link'])
     if pos == -1:
         item['item_id'] = i
