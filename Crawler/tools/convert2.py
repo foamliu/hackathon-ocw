f3753320b5f84ae6e1cc6a4195a3e3ac4141d2e4
刘杨 <foamliu@yeah.net>
2016-05-26 22:06:18 +0800

    爬虫：修复一席数据的问题

--- Crawler/tools/convert2.py
+++ Crawler/tools/convert2.py
@@ -8,8 +8,8 @@ output_file = codecs.open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\FeedAPI
 items = json.load(input_file, encoding='utf-8')
 
 for item in items:
-    if item['source'] == 'InfoQ' and item['tags'] == '互联网':
-        item['tags'] = '互联网 InfoQ'
+    if item['source'] == '一席' and item['piclink'] == '':
+        item['enabled'] = False
 
 
 json.dump(items, output_file, indent=4, ensure_ascii=False, sort_keys=True)
