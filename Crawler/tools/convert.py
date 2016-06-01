4ac050ffbf1af75798053b3ec0f8547ec0c06d17
刘杨 <foamliu@yeah.net>
2016-06-01 23:08:07 +0800

    refresh yixi

--- Crawler/tools/convert.py
+++ Crawler/tools/convert.py
@@ -24,14 +24,14 @@ def getPos(items, link):
             return i
     return -1
 
-input_file_1 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\Crawler\open163exsub\out.json', "r", encoding="utf-8")
+input_file_1 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\Crawler\yixi\out.json', "r", encoding="utf-8")
 input_file_2 = open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\items.json', "r", encoding="utf-8")
 output_file = codecs.open(r'C:\Users\Foam\Documents\GitHub\hackathon-ocw\FeedAPI\app\assets\jsons\output.json', "w", encoding="utf-8")
 
 items = json.load(input_file_2, encoding='utf-8')
 
 lines = input_file_1.readlines()
-i = 36677
+i = 36693
 for line in lines:
     line = line.replace('\\','\\\\')
     #print(line)
