c6cbb6f6138f27a1cfaf44d6c68083d7c3734299
Fan Ouyang <lyixuanoyf@gmail.com>
2016-06-08 08:59:47 +0800

    Add and update courses to mongodb when convert, and add initial_db for initial courses in mongodb

---
+++ Crawler/tools/initial_db.py
@@ -0,0 +1,17 @@
+# initial cources collection
+import json
+import codecs
+import io
+from pymongo import MongoClient
+
+root_path = 'C:\\Users\\Foam\\Documents\\GitHub\\hackathon-ocw\\'
+items_file = open(root_path + 'FeedAPI\\app\\assets\\jsons\\items.json', "r", encoding="utf-8")
+items = json.load(items_file, encoding='utf-8')
+
+client = MongoClient('192.168.0.1', 27017);
+db = client['jiekodb'];
+courses = db['course']
+courses.drop()
+courses.insert_many(items)
+courses.create_index("link");
+client.close()
