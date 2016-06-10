e22728612b0a4ca8dfe5cfce3ed843031f0c6fee
刘杨 <foamliu@yeah.net>
2016-06-10 09:42:49 +0800

    more comments in server side scala source files

--- FeedAPI/app/controllers/Application.scala
+++ FeedAPI/app/controllers/Application.scala
@@ -3,9 +3,6 @@ package controllers
 import scala.collection.JavaConversions.asScalaBuffer
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Await
-import scala.concurrent.Future
-import scala.concurrent.duration.Duration
 import scala.io.Codec.string2codec
 import scala.io.Source
 
@@ -20,12 +17,10 @@ import org.apache.mahout.cf.taste.recommender.Recommender
 import org.apache.mahout.cf.taste.similarity.UserSimilarity
 
 import javax.inject.Inject
-import play.api.Play.current
 import play.api.Logger
 import play.api.Play
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.Play.current
 import play.api.libs.json.JsArray
-import play.api.libs.json.JsObject
 import play.api.libs.json.JsValue
 import play.api.libs.json.JsValue.jsValueToJsLookup
 import play.api.libs.json.Json
@@ -37,27 +32,27 @@ import play.api.mvc.Controller
 import play.modules.reactivemongo.MongoController
 import play.modules.reactivemongo.ReactiveMongoApi
 import play.modules.reactivemongo.ReactiveMongoComponents
-import play.modules.reactivemongo.json.collection.JSONCollection
 import reactivemongo.core.actors.Exceptions.PrimaryUnavailableException
-
+    
 object Application {
-
-    private val howMany = 20
+  
+    private val howMany = 20 // 每次推荐多少条
     private val n = 2 // Nearest N User Neighborhood
     private val item_file = "app/assets/jsons/items.json"
 
+    // MongoDB 相关参数
     private val mongoHost = Play.current.configuration.getString("mongodb.ip")
     private val mongoPort = Play.current.configuration.getInt("mongodb.port")
     private val mongoDBName = Play.current.configuration.getString("mongodb.db")
 
-    private var courses: Seq[Course] = null
-    private var recommender: Recommender = null
-    private var tags: Seq[(String, Int)] = null
+    private var courses: Seq[Course] = null  // 课程列表
+    private var recommender: Recommender = null  // 推荐器
+    private var tags: Seq[(String, Int)] = null  // 标签列表
     
     lazy val reactiveMongoApi = current.injector.instanceOf[ReactiveMongoApi]
     def courseRepo = new backend.CourseRepo(reactiveMongoApi)
-
-
+    def ratingRepo = new backend.RatingRepo(reactiveMongoApi)
+   
     private def getCourses(): Seq[Course] = {
 
         if (courses == null) {
@@ -123,7 +118,7 @@ object Application {
     }
 
     private def recommend(userID: Long): List[Long] = {
-        Logger.debug("userID=%d".format(userID))
+        //Logger.debug("userID=%d".format(userID))
         var list = List[Long]()
 
         try {
@@ -160,11 +155,29 @@ object Application {
         scala.util.Random.shuffle(items.filter(_.tags.contains(tag))).take(howMany)
     }
     
+    private def getItemIDs(userID: Long): ListBuffer[Long] = {
+        val itemIDs: ListBuffer[Long] = new ListBuffer[Long]()
+        ratingRepo.find(Json.obj("user_id" -> userID)).map(ratings => {
+            for (rating <- ratings) {
+                val itemID: Long = (rating \ "item_id").as[Long]
+                itemIDs.+=(itemID) 
+            }
+        }).recover { case PrimaryUnavailableException => Logger.error("Please install MongoDB") }
+
+        itemIDs
+    }
+    
+    /*
+     * 定时任务，每小时执行一次。
+     */
     def refresh() = {
         if (null != getRecommender) {
             val t0 = System.nanoTime()
+            //刷新推荐器
             recommender = createNewRecommender
+            //刷新课程列表
             courses = loadCourses
+            //刷新标签列表
             tags = calculateTags
             val t1 = System.nanoTime()
 
@@ -180,14 +193,58 @@ class Application @Inject() (val reactiveMongoApi: ReactiveMongoApi) extends Con
         //Ok("Your new application is ready.")
         Ok(views.html.index("学啥"))
     }
+    
+    /*
+     * 内容原样返回，用于获取性能测试基线。
+     */
+    def echo(message: String) = Action {
+        Ok(message)
+    }
 
+    /*
+     * 为用户推荐课程，以JSON形式返回。
+     */
     def getCandidates(userID: Long) = Action {
-        Logger.debug(userID toString)
+        //Logger.debug(userID toString)
         val candidates: Seq[Course] = Application.getCandidates(userID)
         val json: JsValue = Json.obj("courses" -> candidates)
         Ok(Json.stringify(json))
     }
 
+    /*
+     * 基于标签进行推荐。
+     */
+    def getCandidatesByTag(id: Long, tag: String) = Action {
+        val candidates: Seq[Course] = Application.getCandidatesByTag(id, tag)
+        val json: JsValue = Json.obj("courses" -> candidates)
+        Ok(Json.stringify(json))
+    }
+
+    /*
+     * 搜索标题中带有指定关键字的公开课。
+     */
+    def search(keyword: String) = Action {
+        val candidates: Seq[Course] = Application.search(keyword)
+        val json: JsValue = Json.obj("courses" -> candidates)
+        Ok(Json.stringify(json))
+    }
+    
+    
+    implicit def tuple2Writes[A, B](implicit a: Writes[A], b: Writes[B]): Writes[Tuple2[A, B]] = new Writes[Tuple2[A, B]] {
+        def writes(tuple: Tuple2[A, B]) = JsArray(Seq(a.writes(tuple._1), b.writes(tuple._2)))
+    }
+    /*
+     * 获取所有公开课的标签，以二元组JSON形式返回，标签名字 -> 标签出现的次数。
+     */
+    def getTags(userID: Long) = Action {
+        val tags = Application.getTags(userID)
+        val json: JsValue = Json.obj("tags" -> tags)
+        Ok(Json.stringify(json))
+    }
+    
+    /*
+     * 客户端汇报崩溃信息。
+     */
     def addCrashReport = Action(parse.json) {
         request =>
             {
@@ -196,47 +253,6 @@ class Application @Inject() (val reactiveMongoApi: ReactiveMongoApi) extends Con
                 Logger.warn(jsonString);
                 Ok("Ok")
             }
-    }
-
-    def echo(message: String) = Action {
-        Ok(message)
-    }
-
-    def search(keyword: String) = Action {
-        val candidates: Seq[Course] = Application.search(keyword)
-        val json: JsValue = Json.obj("courses" -> candidates)
-        Ok(Json.stringify(json))
-    }
-
-    implicit def tuple2Writes[A, B](implicit a: Writes[A], b: Writes[B]): Writes[Tuple2[A, B]] = new Writes[Tuple2[A, B]] {
-        def writes(tuple: Tuple2[A, B]) = JsArray(Seq(a.writes(tuple._1), b.writes(tuple._2)))
-    }
-
-    // BSON-JSON conversions
-    import play.modules.reactivemongo.json._
-    def ratingRepo = new backend.RatingRepo(reactiveMongoApi)
-    private def getItemIDs(userID: Long): ListBuffer[Long] = {        
-        val itemIDs: ListBuffer[Long] = new ListBuffer[Long]()
-        ratingRepo.find(Json.obj("user_id" -> userID)).map(ratings => {
-            for (rating <- ratings) {
-                val itemID: Long = (rating \ "item_id").as[Long]
-                itemIDs.+=(itemID) 
-            }
-        }).recover { case PrimaryUnavailableException => InternalServerError("Please install MongoDB") }
-
-        itemIDs
-    }
-
-    def getTags(userID: Long) = Action {
-        val tags = Application.getTags(userID)
-        val json: JsValue = Json.obj("tags" -> tags)
-        Ok(Json.stringify(json))
-    }
-
-    def getCandidatesByTag(id: Long, tag: String) = Action {
-        val candidates: Seq[Course] = Application.getCandidatesByTag(id, tag)
-        val json: JsValue = Json.obj("courses" -> candidates)
-        Ok(Json.stringify(json))
     }
 }
 
