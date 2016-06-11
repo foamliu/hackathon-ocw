5f76c8575d91d166bf28ebffcb9298bb011a7985
root <foamliu@yeah.net>
2016-06-10 21:48:31 -0400

    refresh guokr include posted field

--- Crawler/guokr/guokr/spiders/spider_guokr.py
+++ Crawler/guokr/guokr/spiders/spider_guokr.py
@@ -57,7 +57,7 @@ class GuokrSpider(scrapy.Spider):
         #ActionChains(self.driver).move_to_element(lastElement).click().perform()
         #lastElement.send_keys(Keys.NULL)
             
-        for i in range(1, 600):
+        for i in range(1, 650):
             self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
             time.sleep(5)
 
@@ -65,20 +65,28 @@ class GuokrSpider(scrapy.Spider):
         for info in hxs.xpath('//*[@id="waterfall"]/div'):
             link = cleanse(info.xpath('a[@data-gaevent="scientific_image:v1.1.1.1:scientific"]/@href').extract())
             if not self.downloaded(link):
-                item = GuokrItem()
-                item['title'] = cleanse(info.xpath('h3/a[@class="article-title"]/text()').extract())
-                item['description'] = cleanse(info.xpath('p[@class="article-summary"]/text()').extract())
-                item['piclink'] = cleanse(info.xpath('a/img/@src').extract())
-                item['courselink'] = u''
-                item['source'] = u'果壳网'
-                item['school'] = u'果壳网'
-                item['instructor'] = cleanse(info.xpath('div/a[1]/text()').extract())
-                item['language'] = u'中文'
-                item['tags'] = cleanse(info.xpath('a[@class="label label-common"]/text()').extract())
-                item['link'] = link
-                item['posted'] = cleanse(info.xpath('div[@class="article-info"]/text()').extract())
-                item['crawled'] = time.strftime('%Y-%m-%d %H:%M', time.localtime())
-                yield item
+                title = cleanse(info.xpath('h3/a[@class="article-title"]/text()').extract())
+                if (title != ''):
+                    try:
+                        item = GuokrItem()
+                        item['title'] = title
+                        item['description'] = cleanse(info.xpath('p[@class="article-summary"]/text()').extract())
+                        item['piclink'] = cleanse(info.xpath('a/img/@src').extract())
+                        item['courselink'] = u''
+                        item['source'] = u'果壳网'
+                        item['school'] = u'果壳网'
+                        item['instructor'] = cleanse(info.xpath('div/a[1]/text()').extract())
+                        item['language'] = u'中文'
+                        item['tags'] = cleanse(info.xpath('a[@class="label label-common"]/text()').extract())
+                        item['link'] = link
+                        item['posted'] = cleanse(info.xpath('normalize-space(div[@class="article-info"])').extract())
+                        if '|' in item['posted']:
+                            item['posted'] = item['posted'].split('|')[1]
+                        item['crawled'] = time.strftime('%Y-%m-%d %H:%M', time.localtime())
+                        yield item
+                    except Exception as err:
+                        print(err)
+                        #break
 
 
 
