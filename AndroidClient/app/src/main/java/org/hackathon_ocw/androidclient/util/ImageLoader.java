5bb2bd6fdfae7f11ab9649e9ded40f6aa9bcf382
刘杨 <foamliu@yeah.net>
2016-06-18 02:40:05 +0800

    网速慢的时候，item的背景图片是灰色的，视觉体验不好，最好能换张比较friendly的图片

--- AndroidClient/app/src/main/java/org/hackathon_ocw/androidclient/util/ImageLoader.java
+++ AndroidClient/app/src/main/java/org/hackathon_ocw/androidclient/util/ImageLoader.java
@@ -33,7 +33,7 @@ public class ImageLoader {
         executorService=Executors.newFixedThreadPool(5);
     }
 
-    final int stub_id = R.drawable.no_image;
+    final int stub_id = R.drawable.placeholder;
     public void DisplayImage(String url, ImageView imageView)
     {
         imageViews.put(imageView, url);
